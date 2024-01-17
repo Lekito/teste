051a399c877e51b00dc2a2ca9ca61043a0a7d46b
Lekito <alexlekito075@gmail.com>
2024-01-17 19:46:48 -0300

    Editando a Header

--- projeto-next/src/app/components/Header/index.tsx
+++ projeto-next/src/app/components/Header/index.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-    Navbar,
-    MobileNav,
-    Typography,
-    Button,
-    IconButton,
-    Card,
-} from "@material-tailwind/react";
+
 
 export default function Header() {
 
@@ -19,54 +12,22 @@ export default function Header() {
         );
     }, []);
 
-    const navList = (
-
-        <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal"
-            >
+    return (
+        <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
+            <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
                 <a href="#" className="flex items-center">
                     Pages
                 </a>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal"
-            >
                 <a href="#" className="flex items-center">
                     Account
                 </a>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal"
-            >
                 <a href="#" className="flex items-center">
                     Blocks
                 </a>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal"
-            >
                 <a href="#" className="flex items-center">
                     Docs
                 </a>
-            </Typography>
-        </ul>
-    );
-    return (
-        <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
-            <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4"></Navbar>
+            </ul>
         </div>
     )
 }
